d8cce585d6d9739d2beaa945ac0587c987d1808e
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-05-22 12:05:33 +1000

    trying to debug assets

--- release.conf.js
+++ release.conf.js
@@ -7,9 +7,9 @@ module.exports = {
 		[
 			'@semantic-release/github',
 			{
-				assets: [
-					{ path: 'build.zip', label: 'Build' },
-					{ path: 'coverage.zip', label: 'Coverage' },
+				"assets": [
+					{ "path": 'build.zip', "label": 'Build' },
+					{ "path": 'coverage.zip', "label": 'Coverage' },
 				],
 			},
 		],
