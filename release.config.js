804e5468d4fc555cb5cb583540847bc8a5041c13
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-05-22 13:07:48 +1000

    feat: fixed dumb mistake so my config is finally accurate

--- release.config.js
+++ release.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+	branches: 'master',
 	repositoryUrl: 'https://github.com/nanoTobyMahnert/react-app',
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		["@semantic-release/github", {"assets": ["coverage.zip", "build.zip"]}],
-        "@semantic-release/git"
+		[
+			'@semantic-release/github',
+			{
+				assets: [
+					{ path: 'build.zip', label: 'Build' },
+					{ path: 'coverage.zip', label: 'Coverage' },
+				],
+			},
+		],
 	],
 };
