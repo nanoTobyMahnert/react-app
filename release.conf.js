8cd939ef11e1ed0d4ef7487e9004e4b791c90671
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-05-22 12:17:10 +1000

    fix: still trying to get assets as part of the release

--- release.conf.js
+++ release.conf.js
@@ -5,13 +5,11 @@ module.exports = {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		[
-			'@semantic-release/github',
-			{
-				"assets": [
-					{ "path": 'build.zip', "label": 'Build' },
-					{ "path": 'coverage.zip', "label": 'Coverage' },
-				],
-			},
+			"@semantic-release/github",
+			{"assets": [
+				{"path":"build.zip", "label":"Build"},
+				{"path":"coverage.zip", "label":"Coverage"}
+			]}
 		],
 	],
 };
